da06986ca5e8a37c137ef94c77930c284aa44736
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2021-08-16 20:48:31 +0300

    Add files via upload

---
+++ kate_data/function_for_spellchecker.py
@@ -0,0 +1,22 @@
+from model.spellchecker import spellCorrect, read_corpus, Vocabulary, CharLM, check
+import torch
+
+
+def load_model():
+    data = read_corpus('/model/vocabulary.txt')
+    V = Vocabulary(data)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = CharLM(V.vocab_size, word_len=V.pad_len, emb_dim=128, hidden_size=64)
+    model_filename = "/model/old_rus_lm.pth"
+    model.load_state_dict(torch.load(model_filename, map_location={'cuda:0': 'cpu'}))
+    correct = spellCorrect(V=V, model=model)
+    return correct
+
+
+def check_p(d, corrected):
+    p = d['p'].split('\n')
+    p, corrected = '\n'.join(check(text, corrected) for text in p)
+    d['p'] = p
+    return d, corrected
+
+corrected = []
